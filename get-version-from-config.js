90962afff4354895f3e5bac40f6e0b2dc514af19
Ralph Meier <ralphmeier79@gmail.com>
2021-05-09 17:41:07 +0200

    fix: change api format

--- get-version-from-config.js
+++ get-version-from-config.js
@@ -1,5 +1,6 @@
 const api = require('./api')
 const tab = require('tab')
+const chalk = require('chalk')
 
 // Input comes from config file like this:
 // {
@@ -14,7 +15,12 @@ const tab = require('tab')
 // }
 module.exports = async ({token} = {}) => {
 
-  const versionsResult = await api.getVersionFromConfig({token})
+  const result = await api.getVersionFromConfig({token})
+  const tabResultFormat = result.map(item => {
+    if (item.type === 'data') return [item.value.owner, item.value.repo, item.value.branch, item.value.version]
+    if (item.type === 'header') return ['', chalk.green(item.value), '', '']
+    return item
+  })
 
   // return json or tabbed table?
   // emit table on cli script and return here a json format?
@@ -25,6 +31,6 @@ module.exports = async ({token} = {}) => {
       {label: 'BRANCH', width: 15, align: 'right'},
       {label: 'TAG', align: 'left'}
     ],
-    'rows': versionsResult
+    'rows': tabResultFormat
   })
 }
